405d9103f1099bbe71c16d38b4a6e20fa24ce4f6
Rakib Hridoy <rakibmahmud2607@gmail.com>
2023-05-05 11:26:10 +0600

    Initial commit of Kotlin project

---
+++ In-App-Update-Kotlin/app/src/main/java/com/example/in_app_update_java/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.in_app_update_java
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
